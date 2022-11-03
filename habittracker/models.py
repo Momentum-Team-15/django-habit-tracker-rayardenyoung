84d4becb6ca301cf43e6d7524e5a45830b87f8b3
Ray Young <ray.arden.young@gmail.com>
2022-11-03 12:56:26 -0400

    forgot to push after un-deleting things fixing bug

--- habittracker/models.py
+++ habittracker/models.py
@@ -14,13 +14,6 @@ class Habit(models.Model):
 class Record(models.Model):
     pass
 
-class Test(models.Model):
-    name = models.CharField(max_length=50)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
 
 class User(AbstractUser):
     pass
--- habittracker/views.py
+++ habittracker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Habit
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'habittracker/index.html')
