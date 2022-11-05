eaececffa31ba1d463d90807264e951964827c4f
Ray Young <ray.arden.young@gmail.com>
2022-11-05 13:43:53 -0400

    updated models

--- habittracker/models.py
+++ habittracker/models.py
@@ -3,17 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
-class Habit(models.Model):
-    name = models.CharField(max_length=50)
-    created_date = models.DateTimeField(auto_now_add=True)  
-
-    def __str__(self):
-        return self.name
-
-class Record(models.Model):
-    pass
-
-
 class User(AbstractUser):
     pass
+
+class Habit(models.Model):
+    name = models.CharField(max_length=255)
+    metric = models.PositiveIntegerField(null=True, blank=True)
+    unit_of_measure = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits", null=True, blank=True)
+
+def __str__(self):
+    return self.name
+
+
+class DailyRecord(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name="records")
+    date = models.DateField(auto_now_add=True)
+    amount = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Record for {self.habit.name}"
