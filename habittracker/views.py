8c21c926c7a4dba5e34c34c56c35429929326c3f
Ray Young <ray.arden.young@gmail.com>
2022-11-06 19:43:53 -0500

    record detail page showing, still cant get pk right

--- habittracker/views.py
+++ habittracker/views.py
@@ -20,6 +20,10 @@ def habit_detail(request, pk):
     habit = Habit.objects.get(pk=pk)
     return render(request, 'habittracker/habit_detail.html', {'habit': habit})
 
+def dailyrecord_detail(request, pk):
+    dailyrecord = DailyRecord.objects.get(pk=pk)
+    return render(request, 'habittracker/dailyrecord_detail.html', {'dailyrecord': dailyrecord})
+
 def create_habit(request):
     if request.method == 'POST':
         #if user is submitting the form
@@ -36,53 +40,43 @@ def create_habit(request):
         #^^^if user is visiting a page with GET request, not submitting the form yet, render a blank
     return render(request, 'habittracker/create_habit.html', {'form': form})
 
-def edit_habit(request, pk):
-    post = get_object_or_404(Habit, pk=pk)
+def edit_habit(request, habitpk):
+    habit = get_object_or_404(Habit, pk=habitpk)
     if request.method == "POST":
-        form = HabitForm(request.POST, request.FILES, instance=post)
+        form = HabitForm(request.POST, request.FILES, instance=habit)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-            return redirect('habit_detail', pk=post.pk)
+            return redirect('habit_detail', habitpk=habit.pk)
     else:
-        form = HabitForm(instance=post)
+        form = HabitForm(instance=habit)
     return render(request, 'habittracker/edit_habit.html', {'form': form})
 
-def delete_habit(request, pk):
-    post = get_object_or_404(Habit, pk=pk)
+def delete_habit(request, habitpk):
+    habit = get_object_or_404(Habit, pk=habitpk)
     if request.method == "POST":
-        post.delete()
+        habit.delete()
         return redirect('home')
     return render(request, 'habittracker/delete_habit.html')
 
 def create_dailyrecord(request):
     if request.method == 'POST':
-        #if user is submitting the form
         form = DailyRecordForm(request.POST, request.FILES)
-        #form is the filled out ("bound") form with user data
         if form.is_valid():
-            #django checks if form is valid (filled out with no missing or mistyped data)
             dailyrecord = form.save()
-            #because it's a ModelForm, saving it will create an instance of Habit in the database
-            #only need commit=False if you are going to add additional data not on the form (like request.user)
             return redirect("home")
     else: 
         form = DailyRecordForm()
-        #^^^if user is visiting a page with GET request, not submitting the form yet, render a blank
     return render(request, 'habittracker/create_dailyrecord.html', {'form': form})
 
 def edit_dailyrecord(request, pk):
-    post = get_object_or_404(DailyRecord, pk=pk)
+    dailyrecord = get_object_or_404(DailyRecord, pk=pk)
     if request.method == "POST":
         form = DailyRecordForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('habit_detail', pk=post.pk)
+            dailyrecord = form.save(commit=False)
+            dailyrecord.save()
+            return redirect('habit_detail', pk=dailyrecord.pk)
     else:
-        form = DailyRecordForm(instance=post)
+        form = DailyRecordForm(instance=dailyrecord)
     return render(request, 'habittracker/edit_dailyrecord.html', {'form': form})
-
-# def dailyrecord_detail(request, pk):
-#     dailyrecord = DailyRecord.objects.get(pk=pk)
-#     return render(request, 'habittracker/dailyrecord_detail.html', {'dailyrecord': dailyrecord})
